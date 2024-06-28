ad83da35e7e14456a3f7ed6e9886d30aee318971
Regarduser <111268408+Regarduser@users.noreply.github.com>
2024-06-28 07:17:30 +0530

    Add files via upload

---
+++ mcq.py
@@ -0,0 +1,71 @@
+print('\n')
+print("-------------------  Welcome to query program ------------------- ")
+print('\n')
+
+marks = 0
+
+print('Q.1 Who developed Python Programming Language? \n a) Wick van Rossum \n b) Rasmus Lerdorf \n c) Guido van Rossum \n d) Niene Stom \n ')
+que = input('Answer : ')
+if que == 'c':
+    marks += 1
+print('\n')
+
+print('Q.2 Which type of Programming does Python support? \n a) object-oriented programming \n b) structured programming \n c) functional programming \n d) all of the menti \n')
+que = input('Answer : ')
+if que == 'a':
+    marks += 1
+print('\n')
+
+print('Q.3 Which of the following is the correct extension of the Python file?\n a) .python\n b) .pl \n c) .py \n d) .p \n')
+que = input('Answer : ')
+if que == 'c':
+    marks += 1
+print('\n')
+
+print('Q.4 All keywords in Python are in _________ \n a) Capitalized \n b) lower case \n c) UPPER CASE \n d) None of the mentioned \n ')
+que = input('Answer : ')
+if que == 'b':
+    marks += 1
+print('\n')
+
+print('Q.5 Which of the following is used to define a block of code in Python language?\n a) Indentation\n b) Key\n c) Brackets\n d) All of the mentioned \n')
+que = input('Answer : ')
+if que == 'a':
+    marks += 1
+print('\n')
+
+print('Q.6 Which of the following character is used to give single-line comments in Python? \n a) // \n b) # \n c) ! \n d) /* \n')
+que = input('Answer : ')
+if que == 'b':
+    marks += 1
+print('\n')
+
+print('Q.7 Which of the following functions is a built-in function in python? \n a) factorial() \n b) print() \n c) seed() \n d) sqrt() \n ')
+que = input('Answer : ')
+if que == 'b':
+    marks += 1
+print('\n')
+
+print('Q.8 Which of the following is not a core data type in Python programming? \n a) Tuples \n b) Lists \n c) Class \n d) Dictionary \n ')
+que = input('Answer : ')
+if que == 'c':
+    marks += 1
+print('\n')
+
+print('Q.9 Which one of the following is not a keyword in Python language?\n a) pass\n b) eval\n c) assert\n d) nonlocal \n ')
+que = input('Answer : ')
+if que == 'b':
+    marks += 1
+print('\n')
+
+print('Q.10 What arithmetic operators cannot be used with strings in Python? \n a) * \n b) % \n c) + \n d) All of the mentioned \n ')
+que = input('Answer : ')
+if que == 'b':
+    marks += 1
+print('\n')
+
+# print('your total marks is : ', marks)
+
+print("                     Total Question = 10                            \n")
+print("                     Obtained marks Out of 10 =", marks,"                      \n")
+print("-------------------  Thankyou for using this program  ------------------- \n")
